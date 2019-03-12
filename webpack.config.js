46fceb3e7f5efe35a8190eb3ce5467b9877a2f43
DaveP <dave@wearecrystal.uk>
2019-03-12 23:21:52 +0000

    Basic webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './src/scripts/main.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+
+  devServer: {
+    port: 3000,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+  }
+  if (argv.mode === 'production') {
+  }
+
+  return config;
+};
